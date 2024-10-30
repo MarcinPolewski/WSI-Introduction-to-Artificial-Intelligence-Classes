608ff076f8d73eeb94466e0ee0a67ad9020bd8ed
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-10-30 20:04:36 +0100

    switched from objects to np, implemented other approach to reproduction

--- task2/solver.py
+++ task2/solver.py
@@ -1,7 +1,6 @@
 import numpy as np
 from abc import ABC, abstractmethod
 from Parameters import Parameters
-from Entity import Entity
 from problem import problem1
 import random
 import copy
@@ -30,126 +29,166 @@ class Genetic_Algorithm(Solver):
     def __init__(self, parameters: Parameters):
         self.parameters = parameters
 
-    def single_point_crossing(self, entity1, entity2):
+    def single_point_crossing(self, genome1, genome2):
         # @TODO starting from 1, because corssing that starts with first element does not make sense ??????
-        crossing_idx = random.randint(1, len(entity1.genome) - 1)
-
-        genome1 = entity1.genome
-        genome2 = entity2.genome
+        crossing_idx = random.randint(1, len(genome1) - 1)
 
         # Perform crossover
-        entity1.genome = np.concatenate(
-            (genome1[:crossing_idx], genome2[crossing_idx:])
-        )
-        entity2.genome = np.concatenate(
-            (genome2[:crossing_idx], genome1[crossing_idx:])
-        )
+        genome1 = np.concatenate((genome1[:crossing_idx], genome2[crossing_idx:]))
+        genome2 = np.concatenate((genome2[:crossing_idx], genome1[crossing_idx:]))
 
-    def selection(self, population):
+    def selection(self, population, scores):
         # roulete selection
-        # @TODO tak jest git ?
-        sum_of_population_values = sum([x.value for x in population])
 
-        new_population = []
-        for entity in population:
-            probability_of_selection = (
-                entity.value / sum_of_population_values
-            )  # value in range from 0 to 1
-            if probability_of_selection > random.random():
-                new_population.append(entity)
+        sum_of_population_values = np.sum(scores)
+        population_size = len(population)
+        new_population = np.empty((len(population), len(population[0])), dtype=bool)
+        # new_population = np.empty((population_size), dtype=np.ndarray)
+        new_population_idx = 0
 
-        while len(new_population) != len(population):
-            entity = np.random.choice(population)
-            if probability_of_selection > random.random():
-                new_population.append(entity)
+        while new_population_idx != population_size:
+            random_value = random.random()  # value from 0 to 1
+            sum_of_prevoius_probabilities = 0
+            idx = 0
+            while (
+                sum_of_prevoius_probabilities < random_value and idx < population_size
+            ):
+                sum_of_prevoius_probabilities += scores[idx] / sum_of_population_values
+                idx += 1
 
-        return np.array(new_population)
+            # now idx is the chosen value
+            new_population[new_population_idx] = population[idx - 1]
+            new_population_idx += 1
 
-    def crossover(self, population, problem):
-        # single-point-crossing
-        entities_to_cross = population.tolist()
-        population = []
+        return new_population
+
+    def crossover(self, population):
+        entities_to_cross = list(population)
+
+        new_population = np.empty((len(population), len(population[0])), dtype=bool)
+        # new_population = np.empty((len(population)), dtype=np.ndarray)
+        new_population_idx = 0
+
         while len(entities_to_cross) >= 1:
-            entity1, entity2 = np.random.choice(
-                entities_to_cross, size=2, replace=False
-            )  # we do not want to cross the same entity with itself
+            idx1, idx2 = random.sample(range(len(entities_to_cross)), 2)
+            genome1 = population[idx1]
+            genome2 = population[idx2]
+
             if self.parameters.probability_of_crossover > random.random():
-                self.single_point_crossing(entity1, entity2)
-                entity1.value = problem.evaluate(entity1.genome)
-                entity2.value = problem.evaluate(entity2.genome)
-            population.append(entity1)
-            population.append(entity2)
-            entities_to_cross.remove(entity1)
-            entities_to_cross.remove(entity2)
+                crossing_idx = random.randint(1, len(genome1) - 1)
+                # Perform crossover
+                genome1 = np.concatenate(
+                    (genome1[:crossing_idx], genome2[crossing_idx:])
+                )
+                genome2 = np.concatenate(
+                    (genome2[:crossing_idx], genome1[crossing_idx:])
+                )
+
+            new_population[new_population_idx] = genome1
+            new_population_idx += 1
+            new_population[new_population_idx] = genome2
+            new_population_idx += 1
+
+            entities_to_cross.pop(max(idx1, idx2))  # Remove the higher index first
+            entities_to_cross.pop(min(idx1, idx2))  # Remove the lower index
+
+            # entities_to_cross = [
+            #     x
+            #     for x in entities_to_cross
+            #     if (x is not population[idx1] and x is not population[idx2])
+            # ]
+            # print()
+
+            # entities_to_cross = list(
+            #     filter(
+            #         lambda x: (x is not population[idx1])
+            #         and (x is not population[idx2]),
+            #         entities_to_cross,
+            #     )  # @TODO FIX this - some times it only deletes one element
+            # )
 
         if len(entities_to_cross) == 1:
-            population.append(entities_to_cross[0])
+            new_population[new_population_idx] = entities_to_cross[0]
 
-        return np.array(population)
+        return new_population
 
-    def mutate(self, population, problem):
+    def mutate(self, population):
 
-        for entity in population:
-            for i in range(len(entity.genome)):
+        for genome in population:
+            for i in range(len(genome)):
                 if self.parameters.probability_of_mutation > random.random():
-                    entity.genome[i] = not entity.genome[i]
-                    entity.value = problem.evaluate(entity.genome)
+                    genome[i] = not genome[i]
 
-    def find_the_best_entity(self, population):
-        return np.max(population)
+    def find_the_best_entity(self, population, scores):
+        best_score_idx = 0
+        population_size = len(population)
+        for i in range(1, population_size):
+            if scores[i] > scores[best_score_idx]:
+                best_score_idx = i
+
+        return population[i], scores[i]
 
     def get_parameters(self):
         return self.parameters
 
-    def get_missing_population_members(self, population, problem, wanted_size):
+    def get_missing_population_members(self, problem, how_many):
         members_to_add = []
-        while len(population) + len(members_to_add) < wanted_size:
-            entity_value = np.random.choice([True, False], size=problem.dimension)
-            members_to_add.append(Entity(entity_value, problem.evaluate(entity_value)))
+        for _ in range(how_many):
+            members_to_add.append(
+                np.random.choice([True, False], size=problem.dimension)
+            )
         return np.array(members_to_add)
 
     def solve(self, problem, initial_solutions=None):
         if initial_solutions is None:
-            initial_solutions = np.array([], dtype=bool)
+            initial_solutions = self.get_missing_population_members(
+                problem, self.parameters.population_size
+            )
 
-        if len(initial_solutions) != self.parameters.population_size:
+        elif len(initial_solutions) != self.parameters.population_size:
             initial_solutions = np.concatenate(
                 (
                     initial_solutions,
                     self.get_missing_population_members(
-                        initial_solutions, problem, self.parameters.population_size
+                        problem,
+                        self.parameters.population_size - len(initial_solutions),
                     ),
                 )
             )
 
         # should we copy population ??
         population = initial_solutions
+        scores = problem.evaluate(population)
 
-        result = copy.copy(self.find_the_best_entity(population))
+        result, result_score = self.find_the_best_entity(population, scores)
 
         for i in range(self.parameters.number_of_generations):
-            S = self.selection(population)
-            C = self.crossover(S, problem)
-            self.mutate(C, problem)
+            S = self.selection(population, scores)
+            C = self.crossover(S)
+            self.mutate(C)
+            scores = problem.evaluate(C)
 
             # find the best
-            best_of_population = self.find_the_best_entity(population)
-            print(problem.evaluate(best_of_population.genome))
+            best_of_population, best_of_population_score = self.find_the_best_entity(
+                population, scores
+            )
+            # print(best_of_population_score)
 
-            if result < best_of_population:
+            if result_score < best_of_population_score:
                 result = copy.copy(best_of_population)
-                print("better found^")
+                result_score = copy.copy(best_of_population_score)
+                # print("better found^")
 
             # succession
             population = C
 
-        return (result, result.value)
+        return (result, result_score)
 
 
 def main():
     print("start..")
 
-    pm = Parameters(1000, 0.75, 0.01, 100)
+    pm = Parameters(10000, 0.75, 0.01, 100)
     gm = Genetic_Algorithm(pm)
     found_value, score = gm.solve(problem1)
     print(score)
